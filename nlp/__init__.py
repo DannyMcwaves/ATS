c40597d180450c3afd3b289ab0d8804144877e75
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-09 19:25:04 +0000

    updating initialisation script

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -3,5 +3,7 @@
 """
 
 from nlp.process_pdf import remove_stop_words, yield_text_from_pdf
+from nlp.database_driver import SqlInterface
+from nlp.pdf_data import using_multiprocess, using_threads
 
-__all__ = ["remove_stop_words", "yield_text_from_pdf"]
+__all__ = ["remove_stop_words", "yield_text_from_pdf", "SqlInterface", "using_multiprocess", "using_threads"]
