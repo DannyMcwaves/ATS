3c7b26dd44d7fe38d0fd90b4da437a597582d808
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-10 10:43:56 +0000

    refactoring the base project

--- nlp/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-## INITIALIZATION SCRIPT
-"""
-
-from nlp.process_pdf import remove_stop_words, yield_text_from_pdf
-from nlp.database_driver import SqlInterface
-from nlp.pdf_data import using_multiprocess, using_threads
-
-__all__ = ["remove_stop_words", "yield_text_from_pdf", "SqlInterface", "using_multiprocess", "using_threads"]
