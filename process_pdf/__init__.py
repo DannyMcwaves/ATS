43c0b9ff39d9e6e2604f0175ffe12756aa696840
dannymcwaves <dannymcwaves96@gmail.com>
2018-02-20 14:20:52 +0000

    changed default export name to read

--- process_pdf/__init__.py
+++ process_pdf/__init__.py
@@ -2,7 +2,7 @@
 ## INITIALIZATION SCRIPT
 """
 
-from process_pdf.read_pdf import remove_stop_words, yield_text_from_pdf
-from process_pdf.pdf_data import using_multiprocess, using_threads
+from .pdf_data import using_multiprocess as read
 
-__all__ = ["remove_stop_words", "yield_text_from_pdf", "SqlInterface", "using_multiprocess", "using_threads"]
+
+__all__ = ["read"]
