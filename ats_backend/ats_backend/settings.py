7a52b714873a151f6d9134f9e14993ab5b135a4d
dannymcwaves <dannymcwaves96@gmail.com>
2018-02-23 19:13:51 +0000

    updated setting to include celery config

--- ats_backend/ats_backend/settings.py
+++ ats_backend/ats_backend/settings.py
@@ -124,3 +124,19 @@ STATIC_URL = '/static/'
 
 # media root where the users uploaded files would be base.
 MEDIA_ROOT = 'resume_db'
+
+
+####################
+# config file for Celery Daemon
+
+# default RabbitMQ broker
+broker_url = 'amqp://'
+
+# using a more persistent database for backend purposes.
+result_backend = 'mongodb://localhost:27017/ats'
+
+# enable utc timezone configuration.
+enable_utc = True
+
+# the color of the worker logs.
+worker_log_color = '#cc3366'
