adc3537d192109b71f5ce43421e3c6d2529a9b42
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-05 09:32:18 +0000

    changed url path configuration to use url regex

--- ats_backend/ats_backend/urls.py
+++ ats_backend/ats_backend/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import include
+from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('ats/', include('ats_api.urls')),
-    path('api/', include('atsApi.urls')),
-    path('', admin.site.urls),
+    url(r'^api/', include('ats_api.urls', namespace='api')),
+    url(r'^doc/', include_docs_urls(title='REST API for DRF')),
+    url(r'', admin.site.urls)
 ]
