b939911336dbe7f19ace0c71f30fadbbb98d18f5
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-29 10:16:05 +0000

    tasks running in separate threads using celery

--- ats_backend/ats_api/tasks.py
+++ ats_backend/ats_api/tasks.py
@@ -1,3 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from rest_framework import status
+from rest_framework.response import Response
+import sys
+import os
+
+sys.path.append('..')
+
+from process_pdf import read
+
+__all__ = ['log_data']
+
+
+@shared_task
+def log_data(data):
+    # print()
+    parsed = read(os.getcwd() + '/' + '/'.join(data['resume'].split('/')[2:]))
+    print(parsed)
+    return Response(data, status=status.HTTP_201_CREATED)
 
