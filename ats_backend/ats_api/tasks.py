05b149e925da0af20108febb784b351f0b847c2c
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-01 11:41:12 +0000

    new celery tasks added for job description processing

--- ats_backend/ats_api/tasks.py
+++ ats_backend/ats_api/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from rest_framework import status
-from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.response import Response
 import sys
 import os
 
@@ -9,13 +9,28 @@ sys.path.append('..')
 
 from process_pdf import read
 
-__all__ = ['log_data']
+__all__ = ['process_info']
 
 
 @shared_task
-def log_data(data):
-    # print()
-    parsed = read(os.getcwd() + '/' + '/'.join(data['resume'].split('/')[2:]))
-    print(parsed)
-    return Response(data, status=status.HTTP_201_CREATED)
+def log(data):
+    print(data)
 
+
+@shared_task
+def read_pdf(data):
+    log(data)
+    return read(os.getcwd() + '/' + '/'.join(data.split('/')[2:]))
+
+
+@shared_task
+def read_job_description(data):
+    return data.split(' ')
+
+
+def process_info(data):
+    pdf = read_pdf(data['resume'])
+    return {
+        'resume_data': [word for page in pdf for word in page],
+        'job_data': read_job_description(data['description'])
+    }
