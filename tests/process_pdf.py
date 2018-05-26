5a8b66fd69038dbeacbfc54726702b62a0e6dfad
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-26 14:46:16 +0000

    test pdf processor

--- tests/process_pdf.py
+++ tests/process_pdf.py
@@ -2,26 +2,20 @@
 main test for process_pdf to see if it is working correctly
 """
 import time
-from process_pdf import using_threads, using_multiprocess
+from process_pdf import read
 PDF_FILE = "../pdf_samples/eddie_jung.pdf"
 
-
-def thread():
-    res_queue = using_threads(PDF_FILE)
-    while not res_queue.empty():
-        result = res_queue.get()
-        print(result)
-
-
-def process():
-    data = using_multiprocess(PDF_FILE)
-    for i in data:
-        print(i)
+#
+# def process():
+#     data = getPdf(PDF_FILE)
+#     for i in data:
+#         print(i)
 
 
 if __name__ == '__main__':
     tt1 = time.time()
-    process()
+    # process()
+    print(read)
     tt2 = time.time()
     print(tt2 - tt1)
 
